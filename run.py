0966b05e6e14c7c334842883d503bda407beeef5
Codie <codie.tackett95@gmail.com>
2023-07-08 15:07:14 -0400

    added more commentation for better understanding

--- run.py
+++ run.py
@@ -1,7 +1,32 @@
 from carrierEstimator import plan_calculator
 
 # Input parameters
-plan_cost = 40
+'''
+plan_cost: more commonly known as line fee. 
+Ex: 45 line fee
+~~~
+
+device_payment: payment for the data service provided by your mobile operator (if any).
+Ex: an iPhone 14 may cost 27.99 per month
+~~~
+
+device_promo: how much should be deducted from your monthly cost
+Ex: You traded in an iphone 11 for the 14 and got a promo/credit for $400 at $10 off every month.
+The $10 would be deducted from your device_payment
+~~~
+
+sales_tax: what percentage of revenue goes to taxes? 
+Ex: In Kentucky it's 6%
+~~~
+
+carrier: name or branding of your cellular provider
+~~~
+
+term: How long will you be paying off this device?
+
+'''
+
+plan_cost = 40 
 device_payment = 0
 device_promo = 0
 sales_tax = 1.06
